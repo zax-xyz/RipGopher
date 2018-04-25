8c070d9ed341d9117d49c11234971f6a6702d79a
Zaxutic <zaxutic@gmail.com>
2018-04-25 22:36:01 +1000

    Cleaned up file, add time to output, fix reconnect issues

--- gopher.py
+++ gopher.py
@@ -1,95 +1,114 @@
 import socket
 import datetime
 import itertools
-
+import time
+import traceback
 
 Host = "irc.twitch.tv"
 Port = 6667
+Owner = 'zaxu__'  # Put username in lowercase
 
 with open("auth", "r", encoding='utf-8') as auth_file:
     line = auth_file.readline().split()
-    Nick = line[0]
+    Nick = line[0].lower()
     Pass = line[1]
     Channel = line[2]
 
-with open("kill_count.txt", "r", encoding='utf-8') as kill_count_file:
-    try:
-        kill_count = int(kill_count_file.readline())
-    except:
-        kill_count = 0
+with open("kill_count.txt", "r", encoding='utf-8') as kill_file:
+    line = kill_file.readline().strip()
+    kill_count = int(line[0] or 0)
+    raw_count = int(line[1] or kill_count)
+
+def current_time():
+    t = datetime.datetime.now()
+    return t.strftime("%y-%m-%d %H:%M:%S ")
 
 def connect():
     global s
-    global line
-    print("CONNECTING")
+    print(current_time(), "CONNECTING")
     s = socket.socket()
     s.connect((Host, Port))
     s.send(bytes("PASS " + Pass + "\r\n", "UTF-8"))
     s.send(bytes("NICK " + Nick + "\r\n", "UTF-8"))
     s.send(bytes("JOIN #" + Channel + " \r\n", "UTF-8"))
-    print("CONNECTED")
+    print(current_time(), "CONNECTED")
     while True:
         line = str(s.recv(1024))
         if "End of /NAMES list" in line:
-            print("Entered " + Channel + "'s chat")
+            print(current_time(), f"Entered {Channel}'s chat")
             break
 
-connect()
+def auto_reconnect():
+    if len(line) == 0:
+        sec = 2
+        for i in itertools.count():
+            try:
+                print(current_time(), f"Reconnecting in {str(secs)} seconds...")
+                s.close()
+                time.sleep(sec)
+                if sec != 16 and i % 2 == 1:
+                    sec *= 2
+                connect()
+                break
+            except:
+                traceback.print_exc()
+                pass
 
 def send_message(msg):
     s.send(bytes("PRIVMSG #" + Channel + " :" + msg + "\r\n", "UTF-8"))
 
-def current_time():
-    now = datetime.datetime.now()
-    return now.strftime("%Y-%m-%d %H:%M")
+def write(history):
+    with open('kill_count.txt', 'w', encoding='utf-8') as kill_count_file:
+        kill_count_file.write(f"{kill_count} {raw_count}")
+    with open('kill_history.txt', 'a', encoding='utf-8') as kill_history_file:
+        kill_history_file.write(f"{current_time()} {history} ({username})\n")
+    print(current_time(), history, f"({username})")
 
-
-while True:
-    line = s.recv(1024).decode("utf-8")
-    if len(line) == 0:
-        for i in itertools.count():
-            try:
-                sys_print_write("Reconnecting in " + str(sec) + " seconds...")
-                s.close()
-                time.sleep(sec * 1000)
-                if sec != 16 and i % 2 == 1:
-                    sec *= 2
-                connect()
-                sec = 2
-                break
-            except:
-                pass
+def parse_commands():
     parts = line.split(':')
     if line.startswith('PING'):
-        print("---RECEIVED PONG---")
         s.send(bytes("PONG\r\n", "UTF-8"))
-        print("---SENT PONG---")
     elif len(parts) > 2:
         message = ':'.join(parts[2:])
         username = parts[1].split("!")[0]
         m_parts = message.split()
-        com = m_parts[0]
+        return message, username, m_parts
+    return None, None, None
 
-        if com == '!gopherkilled':
-            kill_count += 1
-            with open('kill_count.txt', 'w', encoding='utf-8') as kill_count_file, open('kill_history.txt', 'a', encoding='utf-8') as kill_history_file:
-                kill_count_file.write(str(kill_count))
-                kill_history_file.write('{} Gopher killed ({})\n'.format(current_time(), username))
-                print('{} Gopher killed ({})'.format(current_time(), username))
-            send_message("Ninten the vicious murderer has now killed gopher a total of {} times. May he rest in peace. <3".format(kill_count))
-        elif com == '!gophersaved':
-            kill_count -= 1
-            with open('kill_count.txt', 'w', encoding='utf-8') as kill_count_file, open('kill_history.txt', 'a', encoding='utf-8') as kill_history_file:
-                kill_count_file.write(str(kill_count))
-                kill_history_file.write('{} Gopher death undone ({})\n'.format(current_time(), username))
-                print('{} Gopher death undone ({})'.format(current_time(), username))
-            send_message("Somebody has saved gopher from the depths of ninten's wrath. Welcome back to life, gopher. NamineHi")
-        elif com == '!howmanytimesintotalhasnintentheviciousmurdererkilledgopherfromthehundredacrewoodbasedonthechildrensshowwinniethepoohsofar':
-            send_message('Ninten the vicious murderer has killed gopher a total of {} times.'.format(kill_count))
-        elif com == '!setkillcount' and username == 'zax_____':
-            with open('kill_count.txt', 'w', encoding='utf-8') as kill_count_file, open('kill_history.txt', 'a', encoding='utf-8') as kill_history_file:
-                kill_count = m_parts[1]
-                kill_count_file.write(str(kill_count))
-                kill_history_file.write('{} Gopher death_count set to {} ({})\n'.format(current_time(), kill_count, username))
-                print('{} Gopher death_count set to {} ({})'.format(current_time(), kill_count, username))
-            send_message("Set gopher death count to {}.".format(kill_count))
+def handle_commands():
+    global kill_count
+    global raw_count
+    if com == '!gopherkilled':
+        kill_count += 1
+        raw_count += 1
+        write('Gopher killed')
+        send_message(f"Ninten the vicious murderer has now killed gopher "
+            f"a total of {raw_count} times. May he rest in peace. <3")
+    elif com == '!gophersaved':
+        kill_count -= 1
+        write('Gopher death undone')
+        send_message("Gopher has been freed from the depths of ninten's wrath.")
+    elif com in ['!gopherdeaths', '!howmanytimesintotalhasnintenthevicious'
+                 'murdererkilledgopherfromthehundredacrewood'
+                 'basedonthechildrensshowwinniethepoohsofar']:
+        send_message('Ninten the vicious murderer has killed gopher '
+            f'a total of {raw_count} times. (K/S count of {kill_count})')
+    elif com == '!setkillcount' and username == Owner:
+        raw_count = m_parts[1]
+        write('Gopher death_count set to raw_count}')
+        send_message(f"Set gopher death count to {raw_count}.")
+    elif com == '!setkscount' and username == Owner:
+        kill_count = m_parts[1]
+        write(f'Gopher death_count set to {kill_count}')
+        send_message(f"Set gopher death count to {kill_count}.")
+
+if __name__ == '__main__':
+    connect()
+
+    while True:
+        line = s.recv(1024).decode("utf-8")
+        auto_reconnect()
+
+        message, username, m_parts = parse_commands()
+        if message:
+            handle_commands()
